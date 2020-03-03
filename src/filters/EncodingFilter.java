0fd9d5cdaa0df85aeffe8b56b0b696a89dc304e6
minako.isozaki <mii.hiiragi0203@gmail.com>
2020-03-03 09:48:02 +0900

    Add Report update

--- src/filters/EncodingFilter.java
+++ src/filters/EncodingFilter.java
@@ -1,6 +1,6 @@
 package filters;
 
-    import java.io.IOException;
+import java.io.IOException;
 
 import javax.servlet.Filter;
 import javax.servlet.FilterChain;
@@ -10,42 +10,41 @@ import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
 import javax.servlet.annotation.WebFilter;
 
-    /**
-     * Servlet Filter implementation class EncodingFilter
-     */
-    @WebFilter("/*")
-    public class EncodingFilter implements Filter {
+/**
+ * Servlet Filter implementation class EncodingFilter
+ */
+@WebFilter("/*")
+public class EncodingFilter implements Filter {
 
-        /**
-         * Default constructor.
-         */
-        public EncodingFilter() {
-            // TODO Auto-generated constructor stub
-        }
+    /**
+     * Default constructor.
+     */
+    public EncodingFilter() {
+        // TODO Auto-generated constructor stub
+    }
 
         /**
          * @see Filter#destroy()
          */
         public void destroy() {
-            // TODO Auto-generated method stub
+                // TODO Auto-generated method stub
         }
 
         /**
          * @see Filter#doFilter(ServletRequest, ServletResponse, FilterChain)
          */
         public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
-            request.setCharacterEncoding("UTF-8");
-            response.setCharacterEncoding("UTF-8");
+                request.setCharacterEncoding("UTF-8");
+                response.setCharacterEncoding("UTF-8");
 
-            chain.doFilter(request, response);
+                chain.doFilter(request, response);
         }
 
         /**
          * @see Filter#init(FilterConfig)
          */
         public void init(FilterConfig fConfig) throws ServletException {
-            // TODO Auto-generated method stub
+                // TODO Auto-generated method stub
         }
-
 
 }
